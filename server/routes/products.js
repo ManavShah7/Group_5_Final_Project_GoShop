8fba2e43b8dca06f8e70006c519e959c3d631908
krishi <parekh.krishi@northeastern.edu>
2025-12-11 13:13:25 -0500

    created productjs

---
+++ server/routes/products.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+const { verifyToken, isAdmin } = require('../middleware/auth');
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ */
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find().sort({ createdAt: -1 });
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
